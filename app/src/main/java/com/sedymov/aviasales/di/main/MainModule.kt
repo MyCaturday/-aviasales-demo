faaf49b07abc002092074c6a6f868ffcd24e83e6
Yury Sedymov <mycaturday@gmail.com>
2020-07-21 15:49:45 +0600

    switch to kotlin moxy extensions (main presenter)

--- app/src/main/java/com/sedymov/aviasales/di/main/MainModule.kt
+++ app/src/main/java/com/sedymov/aviasales/di/main/MainModule.kt
@@ -1,6 +1,15 @@
 package com.sedymov.aviasales.di.main
 
+import com.sedymov.aviasales.core.interactors.common.LoggingInteractor
+import com.sedymov.aviasales.core.presentation.main.navigation.MainRouter
+import com.sedymov.aviasales.core.presentation.main.presenter.MainPresenter
 import dagger.Module
+import dagger.Provides
 
 @Module
-class MainModule
+class MainModule {
+
+    @Provides
+    @PerMain
+    internal fun mainPresenter(loggingInteractor: LoggingInteractor, mainRouter: MainRouter) = MainPresenter(loggingInteractor, mainRouter)
+}
